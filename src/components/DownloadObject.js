cb276889c388ed6cf82ed36a0c4322921c466178
asdofe <asdofe@gmail.com>
2017-10-07 01:58:33 +0000

    feat(download_table): download railway table and store as json file

--- src/components/DownloadObject.js
+++ src/components/DownloadObject.js
@@ -8,42 +8,76 @@ var options = {
   path: '/json/'
 };
 var fs = require('fs');
+var AdmZip = require('adm-zip');
+var create_dir_if_not_exists = (path)=>{ if(!fs.existsSync(path)){ fs.mkdirSync(path);}};
 var download_railway_table = function(path)
 {
-    options.path = path;
-    print('path: ' + path);
-    http.get(options, function(res){
-        var tmp_name = path.replace(/\//g, '');
-        print(tmp_name);
-        var file = fs.createWriteStream(tmp_name);
-        res.pipe(file);
-    }).on('error', function(e){
-        console.log('[error]Download json file error: ' + e.message);
+    return new Promise(function(resolve, reject){
+        options.path = path;
+	var data = [], dataLen = 0;
+        http.get(options, (res)=>{
+	   res.on('data', (chunk) =>
+           {
+               data.push(chunk);
+	       dataLen += chunk.length;
+
+	   }).on('end', () =>
+	   {
+	       var buf = new Buffer(dataLen);
+	       for(var i=0, len = data.length, pos = 0; i < len; i++)
+	       {
+		   data[i].copy(buf, pos);
+		   pos += data[i].length;
+	       }
+	       var zip = new AdmZip(buf);
+
+	       var root_dir = './tmp';
+	       create_dir_if_not_exists(root_dir);
+	       file_name = /\/\w+\/(\w+)\.zip/g.exec(path);
+	       var json_content = zip.readAsText(file_name[1] + '.json');
+
+	       print(json_content);
+	       var full_path = root_dir + '/' + file_name[1] + '.json';
+	       print(full_path);
+	       var file = fs.createWriteStream(full_path);
+	       file.write(json_content); 
+	       file.on('finish', () => {
+		   file.close();
+		   resolve();
+	       });
+	   });
+	});
+        
     });
 };
 
 http.get(options, function(res) {
   console.log("Got response: " + res.statusCode);
+  var data = [], dataLen = 0;
   res.on("data", function(chunk) {
     //console.log("BODY: " + chunk);
-    const $ = cheerio.load(chunk);
+       data.push(chunk);
+       dataLen += chunk.length;
+  }).on('end', () => {
+    var buf = new Buffer(dataLen);
+    for (var i=0, len = data.length, pos = 0; i < len; i++) 
+    { 
+	data[i].copy(buf, pos); 
+	pos += data[i].length; 
+    } 
+    const $ = cheerio.load(buf);
     target_link = $('pre a');
-    var flag = 0;
+    print('link num: ' + $(target_link).length);
     $(target_link).each(function(i, link){
         var s= $(this).attr('href');
-        if(s.indexOf('zip') > -1 && flag == 0)
+        if(s.indexOf('zip') > -1) 
         {
-            flag = 1;
-            download_railway_table(s);
-            print(s + '\n');
+            var prom = download_railway_table(s);
         }
 
- 
+      });
+  }).on('error', function(e) {
+      console.log("Got error: " + e.message);
     });
-  });
-}).on('error', function(e) {
-  console.log("Got error: " + e.message);
 });
-
-
 
